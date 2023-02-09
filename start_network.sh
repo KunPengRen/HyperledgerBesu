3077313066f5f509d724c72d4ec659de2fc1820c
KunpengRen <dcsrenk@nus.edu.sg>
2023-02-09 18:12:06 +0800

    add start stop script

--- start_network.sh
+++ start_network.sh
@@ -1,11 +1,4 @@
-### Config and Wallet Accounts for Hyperledger Besu Multi-Node Setup
-
-* [Hyperledger Besu Private Network using Docker](https://www.polarsparc.com/xhtml/BesuPrivateCliqueDocker.html)
-
-### How to run
-
-```
-bash
+./stop_network.sh
 export BESU_HOME=`pwd`
 
 docker run --rm --name bank -d --network host -e BESU_OPTS="--add-opens java.base/jdk.internal.misc=ALL-UNNAMED -Dio.netty.tryReflectionSetAccessible=true" -u $(id -u $USER):$(id -g $USER) -v $BESU_HOME/data/bank:/opt/besu/data -v $BESU_HOME/data/bank:/opt/besu/keys -v $BESU_HOME/data/bank:/opt/besu/public-keys -v $BESU_HOME/conf/bank-config.toml:/config/config.toml -v $BESU_HOME/conf/genesis.json:/config/genesis.json hyperledger/besu:22.7.7 --config-file=/config/config.toml
@@ -15,4 +8,3 @@ docker run --rm --name buyer -d --network host -e BESU_OPTS="--add-opens java.ba
 docker run --rm --name dealer -d --network host -e BESU_OPTS="--add-opens java.base/jdk.internal.misc=ALL-UNNAMED -Dio.netty.tryReflectionSetAccessible=true" -u $(id -u $USER):$(id -g $USER) -v $BESU_HOME/data/dealer:/opt/besu/data -v $BESU_HOME/data/dealer:/opt/besu/keys -v $BESU_HOME/conf/dealer-config.toml:/config/config.toml -v $BESU_HOME/conf/genesis.json:/config/genesis.json hyperledger/besu:22.7.7 --config-file=/config/config.toml
 
 docker run --rm --name dmv -d --network host -e BESU_OPTS="--add-opens java.base/jdk.internal.misc=ALL-UNNAMED -Dio.netty.tryReflectionSetAccessible=true" -u $(id -u $USER):$(id -g $USER) -v $BESU_HOME/data/dmv:/opt/besu/data -v $BESU_HOME/data/dmv:/opt/besu/keys -v $BESU_HOME/conf/dmv-config.toml:/config/config.toml -v $BESU_HOME/conf/genesis.json:/config/genesis.json hyperledger/besu:22.7.7 --config-file=/config/config.toml
-```
